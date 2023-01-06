8e09da51723ca88d48f9b419f3207eaacf7af1c4
Eessh <eesshkumarjagana@gmail.com>
2023-01-06 22:41:35 +0530

    added Test, Error data structures for easy testing

---
+++ tests/tests/Heading3.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include "../../MarkdownParser.hpp"
+#include "../utils.hpp"
+
+void Heading3() {
+	MarkdownParser::Lexer lexer;
+	const std::string md = "### Hola";
+	std::vector<MarkdownParser::Token*> v = lexer.parseMarkdownStrToTokens(md);
+
+	Utils::Test test("Heading3", "Checking lexer for parsing level-3 heading token: \"### Hola\"");
+
+	MarkdownParser::HeadingToken* t = nullptr;
+	t = dynamic_cast<MarkdownParser::HeadingToken*>(v[0]);
+	if (!t) {
+		test.addError(new Utils::RuntimeError("Unable to dynamic_cast Token* to HeadingToken*"));
+		return;
+	}
+
+	if (t->type() != MarkdownParser::TokenType::HEADING)
+		test.addError(new Utils::ValueError("type", "HEADING", MarkdownParser::tokenTypeToStr(t->type())));
+	if (t->level() != 3)
+		test.addError(new Utils::ValueError("level", "3", std::to_string(t->level())));
+	if (t->text() != "Hola")
+		test.addError(new Utils::ValueError("text", "Hola", t->text()));
+	if (t->render() != "<h3>Hola</h3>")
+		test.addError(new Utils::ValueError("render", "<h3>Hola</h3>", t->render()));
+
+	test.log();
+}
--- tests/utils.hpp
+++ tests/utils.hpp
@@ -5,8 +5,191 @@
 #include <string>
 #include "../termcolor/termcolor.hpp"
 
-void Utils__printTestDescription(const std::string& testName, const std::string& testDesc) {
-	std::cout << "";
+namespace Utils {
+
+	enum class ErrorType {
+		RUNTIME,
+		VALUE,
+	};
+
+	class Error {
+	public:
+		virtual const ErrorType& type() const;
+
+	protected:
+		ErrorType mType;
+	};
+
+	class RuntimeError: public Error {
+	public:
+		RuntimeError(const std::string& description);
+
+		const std::string& description() const;
+		std::string& description();
+
+	private:
+		std::string mDescription;
+	};
+
+	class ValueError: public Error {
+	public:
+		ValueError(const std::string& key, const std::string& expectedValue, const std::string& value);
+
+		const std::string& key() const;
+		std::string& key();
+
+		const std::string& expectedValue() const;
+		std::string& expectedValue();
+
+		const std::string& value() const;
+		std::string& value();
+
+	private:
+		std::string mKey;
+		std::string mExpectedValue;
+		std::string mValue;
+	};
+
+	class Test {
+	public:
+		Test(const std::string& name);
+		Test(const std::string& name, const std::string& description);
+
+		const std::string& name() const;
+		std::string& name();
+
+		const std::string& description() const;
+		std::string& description();
+
+		const std::vector<Error*>& errors() const;
+		void addError(Error* error);
+
+		void log() const;
+
+	private:
+		std::string mName;
+		std::string mDescription;
+		std::vector<Error*> mErrors;
+
+		void logPassed() const;
+		void logFailed() const;
+	};
+
+};
+
+
+const Utils::ErrorType& Utils::Error::type() const {
+	return this->mType;
+}
+
+Utils::RuntimeError::RuntimeError(const std::string& description) {
+	this->mType = Utils::ErrorType::RUNTIME;
+	this->mDescription = description;
+}
+
+const std::string& Utils::RuntimeError::description() const {
+	return this->mDescription;
+}
+
+std::string& Utils::RuntimeError::description() {
+	return this->mDescription;
+}
+
+Utils::ValueError::ValueError(const std::string& key, const std::string& expectedValue, const std::string& value) {
+	this->mType = Utils::ErrorType::VALUE;
+	this->mKey = key;
+	this->mExpectedValue = expectedValue;
+	this->mValue = value;
+}
+
+const std::string& Utils::ValueError::key() const {
+	return this->mKey;
+}
+
+std::string& Utils::ValueError::key() {
+	return this->mKey;
+}
+
+const std::string& Utils::ValueError::expectedValue() const {
+	return this->mExpectedValue;
+}
+
+std::string& Utils::ValueError::expectedValue() {
+	return this->mExpectedValue;
+}
+
+const std::string& Utils::ValueError::value() const {
+	return this->mValue;
+}
+
+std::string& Utils::ValueError::value() {
+	return this->mValue;
+}
+
+
+Utils::Test::Test(const std::string& name)
+: mName(name)
+{}
+
+Utils::Test::Test(const std::string& name, const std::string& description)
+: mName(name), mDescription(description)
+{}
+
+const std::string& Utils::Test::name() const {
+	return this->mName;
+}
+
+std::string& Utils::Test::name() {
+	return this->mName;
+}
+
+const std::string& Utils::Test::description() const {
+	return this->mDescription;
+}
+
+std::string& Utils::Test::description() {
+	return this->mDescription;
+}
+
+const std::vector<Utils::Error*>& Utils::Test::errors() const {
+	return this->mErrors;
+}
+
+void Utils::Test::addError(Utils::Error* error) {
+	this->mErrors.push_back(error);
+}
+
+void Utils::Test::log() const {
+	if (this->mErrors.empty())
+		this->logPassed();
+	else
+		this->logFailed();
+}
+
+void Utils::Test::logPassed() const {
+	std::cout << termcolor::on_bright_green << termcolor::bright_white << "Passed" << termcolor::reset << " " << this->mName << "\n";
+	std::cout << "Description: " << this->mDescription << "\n";
+}
+
+void Utils::Test::logFailed() const {
+	std::cout << termcolor::on_bright_red << termcolor::bright_white << "Failed" << termcolor::reset << " " << this->mName << "\n";
+	std::cout << "Description: " << this->mDescription << "\n";
+	if (this->mErrors.empty())
+		return;
+	std::cout << termcolor::red << "Errors:\n";
+	for (Error* error: this->mErrors) {
+		if (error->type() == Utils::ErrorType::RUNTIME) {
+			Utils::RuntimeError* rerror = dynamic_cast<Utils::RuntimeError*>(error);
+			std::cout << "RuntimeError: " << rerror->description() << "\n";
+		}
+		else {
+			Utils::ValueError* verror = dynamic_cast<Utils::ValueError*>(error);
+			std::cout << "Value of " << verror->key() << " does not match with that of expected value:\n";
+			std::cout << "  Expected Value: " << verror->expectedValue() << "\n";
+			std::cout << "  Returned Value: " << verror->value() << "\n";
+		}
+	}
+	std::cout << termcolor::reset;
 }
 
 void Utils__logPassedTest(const std::string& testName, const std::vector<std::string>& errorDesc = {}) {
