a6bf19611d24da8815d2707dc7a69a742e48685e
Eessh <eesshkumarjagana@gmail.com>
2023-01-06 17:39:20 +0530

    added escape codes in build_testbuilder.sh

--- scripts/macos/build_testbuilder.sh
+++ scripts/macos/build_testbuilder.sh
@@ -5,4 +5,4 @@ echo Compiling TestsBuilder ...
 g++ --std=c++2a -O3 ./TestBuilder.cpp -o ./MarkdownParser__TestsBuilder
 
 echo Done.
-echo From next time can just run Test, there\'s no need to compile TestBuilder again :)
+echo From next time can just run Test, there\'s no need to compile TestBuilder again \:\)
